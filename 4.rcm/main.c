a2403e588c5f09c89d9ec0d55dea484b134ec03d
kadimitra <kadimitra@ece.auth.gr>
2020-09-27 19:43:17 +0300

    Add bandwidth calculation

--- 4.rcm/main.c
+++ 4.rcm/main.c
@@ -21,6 +21,7 @@ double p_time;
 
 Queue * createQueue(int maxElements);
 int *rcm(int *M, int n, Queue *Q, Queue *R);
+int find_bandwidth(int *X, int n);
 
 int main(){
 
@@ -55,14 +56,16 @@ int main(){
       M[n*i + j] = M[n*j + i];
   }
 
-  // Print created matrix
-  /*for(int i=0; i<n; i++){
-      for(int j=0; j<n; j++){
-        printf("%d ", M[n*i + j]);
-      }
-      printf("\n");
+  /*for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      printf("%d",M[n*i+j]);
+    }
+    printf("\n");
   }
   printf("\n");*/
+
+  // Calculate bandwith before the permutation
+  int bandwith_before = find_bandwidth(M,n);
 
   // Instantiate an empty queue Q and empty array for permutation order of the objects R.
 	Queue *Q = createQueue(n*n);
@@ -74,16 +77,78 @@ int main(){
 
   gettimeofday (&endwtime, NULL);
   p_time = (double)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6 + endwtime.tv_sec - startwtime.tv_sec);
-  printf("Returned in %fsec\n",p_time);
+  printf("RCM algorithm returned in %fsec\n",p_time);
 
-  //Print result
-  /*for(int i=0; i<n; i++){
-    printf("%d ", R[i]);
+  /*for(int i=0;i<n;i++){
+    printf("%d",Result[i]);
+  }
+  printf("\n\n");*/
+
+  // Create the final array by applying the permutations as they are shown in Result
+  int *F = malloc(n*n * sizeof(int)); //final matrix
+  int permutation = -1;
+  for(int i=0;i<n;i++){
+    permutation = Result[i];
+    for(int j=0;j<n;j++){
+      F[n*i+j] = M[n*permutation+j];
+    }
+  }
+
+  /*for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      printf("%d",F[n*i+j]);
+    }
+    printf("\n");
   }
   printf("\n");*/
+
+  // Calculate bandwith after the permutation
+  int bandwith_after = find_bandwidth(F,n);
+
+  printf("Bandwith before the RCM algorith is %d, while after the permutations is %d.\n",bandwith_before,bandwith_after);
 
   free(R);
   free(M);
 
   return 0;
+}
+
+int find_bandwidth(int *X, int n)
+{
+	int band_hi = 0;
+	int band_lo = 0;
+	int temp;
+
+	for (int i = 0; i < n; i++)
+	{
+		temp = 0;
+		for (int j = n - 1; j > i; j--)
+			if (X[n * i + j] != 0)
+			{
+				temp = j - i;
+				break;
+			}
+			else
+				continue;
+
+		if (temp > band_hi)
+			band_hi = temp;
+
+		temp = 0;
+		for (int j = 0; j < i; j++)
+			if (X[n * i + j] != 0)
+			{
+				temp = i - j;
+				break;
+			}
+			else
+				continue;
+
+		if (temp > band_lo)
+			band_lo = temp;
+	}
+
+	int bandwidth = band_lo + band_hi + 1;
+
+	return bandwidth;
 }
